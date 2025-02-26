7376c5572a05bf61db1eef9ee9845c4c73669c7a
Ahmad Wahyudi <ahmadwahyudi2395@gmail.com>
2025-02-26 17:02:16 +0700

    TEST

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // src/utils/api.js
 import axios from "axios"
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || ""
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || ""
 
 export const getGuestBySlug = async (slug) => {
 	try {
