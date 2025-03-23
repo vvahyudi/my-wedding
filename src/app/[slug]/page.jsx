0ea0ace964bf1a2897984ee2c6dcac3516291b79
Ahmad Wahyudi <ahmadwahyudi2395@gmail.com>
2025-03-23 23:53:49 +0700

    ft: add html entity if ' appear

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -138,7 +138,7 @@ export default function Home() {
 							</div>
 						) : data && data.data ? (
 							<h1 className={`text-2xl font-extrabold italic py-2`}>
-								{data.data.name}
+								{data.data.name.replace(/'/g, "&#39;")}
 							</h1>
 						) : null}
 					</div>
